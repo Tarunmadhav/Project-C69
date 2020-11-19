034dbd780227fb1e28f416a505b4221c752c2b51
Tarun Madhav <65611329+Tarunmadhav@users.noreply.github.com>
2020-11-19 12:51:25 +0530

    Add files via upload

---
+++ screens/ScanScreen.js
@@ -0,0 +1,45 @@
+import {image, View} from 'react-native'
+export default class ScanScreen extends React.Component{
+    cunstructor(){
+    super();
+    this.state = {
+        hasCameraPermissions:null,
+        scanned:false,
+        scannedData:'',
+        buttonState:'normal'
+    }
+    }
+ 
+    getCameraPermissions=async()=>{
+        const {status}=await Permissions.askAsync(Permissions.CAMERA)
+        this.setState({hasCameraPermissions:status==="granted",buttonState:"clicked"})
+      }
+      handleBarCodeScanned=async({type,data})=>{
+     this.setState({scanned:true,scannedData:data,buttonState:"normal"})
+      }
+      render(){
+        const hasCameraPermissions=this.state.hasCameraPermissions
+        const scanned=this.state.scanned
+        const buttonState=this.state.buttonState
+        if(buttonState==="clicked"&&hasCameraPermissions){
+          return(
+            <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}/>
+          )
+        }
+        else if(buttonState==="normal"){
+          return (
+           <View>
+              <TouchableOpacity style={styles.scanButton} onPress={this.getCameraPermissions} title="Bar Code Scanner">
+   <Text style={styles.buttonText}>Scan QR Code</Text>
+              </TouchableOpacity>
+    
+            </View>
+          )
+        }
+        
+      }
+    
+      }
+    
+      
